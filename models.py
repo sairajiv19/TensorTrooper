6407121c146df3f58dd6505e69c772694e6f9f1f
S Sai Rajiv <myrealme2pro92@gmail.com>
2025-09-10 06:25:49 +0530

    Adding models

---
+++ models.py
@@ -0,0 +1,7 @@
+"""
+This file contains all the data models that will used in the future.
+Let's have strict type hinting and checking for better workflow.
+
+Last Modified: 10-09-2025
+Author: Popcorn
+"""
